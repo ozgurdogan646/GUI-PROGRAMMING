25189ab21ccff4790ee69f1ae70394924dc86251
ozgurdogan646 <ozgurdogan646@gmail.com>
2019-09-26 14:28:49 +0300

    My First Android Application

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
