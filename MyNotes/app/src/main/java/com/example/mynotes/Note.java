df1747f65cc96bc9714389d03c3f81562ce25a0e
ozgurdogan646 <ozgurdogan646@gmail.com>
2019-12-09 00:57:14 +0300

    MyNotes

---
+++ MyNotes/app/src/main/java/com/example/mynotes/Note.java
@@ -0,0 +1,35 @@
+package com.example.mynotes;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.Exclude;
+
+public class Note {
+    @Exclude
+    private String id;
+    private Timestamp date;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
